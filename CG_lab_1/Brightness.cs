dfb76f435b039dac0c6395347ba0cec51df4e6c5
Semyon1104 <sema_titov_04@mail.ru>
2024-02-22 22:59:52 +0300

    add some filters

---
+++ CG_lab_1/Brightness.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CG_lab_1
+{
+    internal class Brightness:Filters
+    {
+        protected override Color calculateNewPixelColor(Bitmap sourceImage, int x, int y)
+        {
+            Color sourceColor = sourceImage.GetPixel(x, y);
+            int c = 50;
+            Color resultColor = Color.FromArgb(Clamp(sourceColor.R + c, 0, 255), Clamp(sourceColor.G + c, 0, 255), Clamp(sourceColor.B + c, 0, 255));
+            return resultColor;
+        }
+    }
+}
